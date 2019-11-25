b244083a581c218e6a005a5f042b265718422fb2
Luis David Morales <luisdavidmorales95@gmail.com>
2019-11-25 17:49:07 -0600

    Definicion de clases

---
+++ clases.php
@@ -0,0 +1,19 @@
+<?php
+//Una clase es nuestro molde de trabajo
+//para nuestros metodos y atributos
+
+    class miClase{
+        //definir atributos
+
+
+        //definir metodos
+
+    }
+
+    class MiClase2{
+        //
+    }
+
+//podemos declarar cualquier numero de clases dentro de un archivo php
+
+?>
